57c60c085352a4065263da50604b10f9cdfc5d37
skmarket <skmarket1998@gmail.com>
2022-07-19 22:21:03 +0900

    first commit

---
+++ trello-clone/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Shimple Trello</h1>
+      </header>
+    </div>
+  )
+}
---
+++ trello-clone/src/components/task/TaskCards.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { AddTaskCardButton } from './button/AddTaskCardButton'
+import { TaskCard } from './TaskCard'
+
+export const TaskCards = () => {
+  const [taskCardsList,setTaskCardsList] = useState([{
+    id: "0",
+    draggableId: "item0",
+  }
+  ]);
+  return (
+    <DragDropContext>
+      <Droppable 
+      droppableId='droppable' 
+      direction='horizontal'>
+        {(provided) => (
+          <div 
+            className='taskCardsArea' 
+            {...provided.droppableProps} 
+            ref={provided.innerRef}
+          >
+            {taskCardsList.map((taskCard,index) => (
+              <TaskCard 
+              key={taskCard.id}  
+              index={index}
+              taskCardsList={taskCardsList}
+              setTaskCardsList={setTaskCardsList}
+              taskCard={taskCard}
+              />
+            ))}
+            {provided.placeholder}
+            <AddTaskCardButton 
+            taskCardsList={taskCardsList} 
+            setTaskCardsList={setTaskCardsList} />
+          </div>
+
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
